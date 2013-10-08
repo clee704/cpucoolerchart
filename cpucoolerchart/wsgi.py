87c6cf3b5beaaef90d275f980aa314399b18aafb
Choongmin Lee <choongmin@me.com>
2013-10-09 02:46:07 +0900

    Remove index page cache when web server starts

--- cpucoolerchart/wsgi.py
+++ cpucoolerchart/wsgi.py
@@ -1,2 +1,9 @@
 from .app import create_app
 application = create_app()
+
+# Code that only executed when a web server starts, not
+# for workers or management scripts.
+
+# Purge the index page cache
+from .extensions import cache
+cache.delete('view//')
