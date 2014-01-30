0190dd2f7d37966e20dad99e3a3723dd8bd17639
Choongmin Lee <choongmin@me.com>
2014-01-31 04:33:35 +0900

    Fix failing tests

--- tests/views_test.py
+++ tests/views_test.py
@@ -48,7 +48,7 @@ class TestViews(object):
         self.app.config['HEROKU_APP_NAME'] = 'foobar'
 
         r = self.client.post('/update')
-        assert r.status_code == 200
+        assert r.status_code == 202
         data = json.loads(to_native(r.data))
         assert data['msg'] == 'process started'
         heroku.from_key.assert_called_with('12345678')
@@ -56,7 +56,7 @@ class TestViews(object):
         assert cpucoolerchart.views.is_update_running()
 
         r = self.client.post('/update')
-        assert r.status_code == 200
+        assert r.status_code == 202
         data = json.loads(to_native(r.data))
         assert data['msg'] == 'already running'
         assert heroku.from_key.call_count == 0
@@ -64,7 +64,7 @@ class TestViews(object):
         crawler.unset_update_running()
         cpucoolerchart.views.is_update_needed.return_value = False
         r = self.client.post('/update')
-        assert r.status_code == 200
+        assert r.status_code == 202
         data = json.loads(to_native(r.data))
         assert data['msg'] == 'already up to date'
         assert heroku.from_key.call_count == 0
